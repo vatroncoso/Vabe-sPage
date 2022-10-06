b36e8b68c003e205f8b7498351b11ab2a968ee75
Valentina Troncoso <vatroncosogarrido@gmail.com>
2022-10-06 11:49:52 +0200

    Avance mañana jueves 6/10

--- myScript.js
+++ myScript.js
@@ -1,32 +1,45 @@
 /* Hobber nav-bar itemes*/
-var linkClicked = document.getElementsByClassName('nav-link');
-var numClass = linkClicked.length;
+	var linkClicked = document.getElementsByClassName('nav-link');
+	var numClass = linkClicked.length;
 
-for (var i = 0; i < numClass; i++) {
-		linkClicked[i].addEventListener('click', function(){
-      var onTheMoment = document.getElementsByClassName('active');
-			onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
-			this.className += ' active';
-    }, false);
-	}
+	for (var i = 0; i < numClass; i++) {
+			linkClicked[i].addEventListener('click', function(){
+		var onTheMoment = document.getElementsByClassName('active');
+				onTheMoment[0].className = onTheMoment[0].className.replace(' active', '');
+				this.className += ' active';
+		}, false);
+		}
 
 /* Scroll to top button*/
-// Get the button:
-let mybutton = document.getElementById("myBtn");
+	// Get the button:
+	let mybutton = document.getElementById("myBtn");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+	// When the user scrolls down 20px from the top of the document, show the button
+	window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+	function scrollFunction() {
+	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+		mybutton.style.display = "block";
+	} else {
+		mybutton.style.display = "none";
+	}
+	}
 
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0; // For Safari
-  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-}
+
+	// When the user clicks on the button, scroll to the top of the document
+	function topFunction() {
+	document.body.scrollTop = 0; // For Safari
+	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+	}
+
+/*Flaoting menu
+$(window).scroll(function(e){ 
+	var $el = $('.nav2'); 
+	var isPositionFixed = ($el.css('position') == 'fixed');
+	if ($(this).scrollTop() > 200 && !isPositionFixed){ 
+	  $el.css({'position': 'fixed', 'top': '0px'}); 
+	}
+	if ($(this).scrollTop() < 200 && isPositionFixed){
+	  $el.css({'position': 'static', 'top': '0px'}); 
+	} 
+  });*/
