647df1df698c5a22b7f7b6685937d1095684cefb
Valentina Troncoso <vatroncosogarrido@gmail.com>
2022-10-10 17:25:32 +0200

    Responsivity almost ready, scroll fixed

--- myScript.js
+++ myScript.js
@@ -10,39 +10,29 @@
 		}, false);
 		}
 
-/* Scroll to top button*/
-	// Get the button:
-	let mybutton = document.getElementById("myBtn");
+/* Scroll to top and menu button  */
+	
+let mybutton = document.getElementById("myBtn");
+let mybutton2 = document.getElementById("menu");
 
-	// When the user scrolls down 20px from the top of the document, show the button
-	window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction()};
 
-	function scrollFunction() {
-	if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-		mybutton.styles.display = "block";
+function scrollFunction() {
+	if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+		mybutton.style.display = "block";
+		mybutton2.style.display = "block";
+
+	
 	} else {
-		mybutton.styles.display = "none";
+		mybutton.style.display = "none";
+		mybutton2.style.display = "none";
 	}
-	}
-
-
-	// When the user clicks on the button, scroll to the top of the document
-	function topFunction() {
-	document.body.scrollTop = 0; // For Safari
-	document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
-	}
-
-/*Flaoting menu
-$(window).scroll(function(e){ 
-	var $el = $('.nav2'); 
-	var isPositionFixed = ($el.css('position') == 'fixed');
-	if ($(this).scrollTop() > 200 && !isPositionFixed){ 
-	  $el.css({'position': 'fixed', 'top': '0px'}); 
-	}
-	if ($(this).scrollTop() < 200 && isPositionFixed){
-	  $el.css({'position': 'static', 'top': '0px'}); 
-	} 
-  });*/
+}
+	
+function topFunction() {
+document.body.scrollTop = 0; // For Safari
+document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 
 /*Contact form
   const $form = document.querySelector('#form')
